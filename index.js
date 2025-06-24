69b9d9eac2c3f7ff85b190f68f5c998bf844edf3
Sidd-D-Ace <siddhantdongre50@gmail.com>
2025-06-25 01:43:22 +0530

    Fixed Edit Post Logic and added a page to view Post

--- index.js
+++ index.js
@@ -17,16 +17,26 @@ app.get('/',(req,res)=>{
     res.render('index.ejs',{postArr});
 })
 
+// renders the post form page
 app.post('/post',(req,res)=>{
     res.render('postLayout.ejs');
 })
 
+// renders the post form page but with selected post content
 app.post('/edit',(req,res)=>{
-    console.log(req.body)
-    // let editPostName=req.body['element']
-    res.render('postLayout.ejs',{})
+    console.log(`Name: ${req.body['element']}`);
+    let editPostName=req.body['element'];          //retrive the name of the post
+    let editPostmessage;
+    fs.readFile(__dirname+'/PostsFile/'+editPostName+'post.txt','utf-8',(err,data)=>{
+        if (err) throw err;
+        console.log(`Successfully Read ${editPostName} Content`);
+        editPostmessage=data;
+        console.log(`Data in data : ${data}`);
+        res.render('postLayout.ejs',{name:editPostName, message:editPostmessage});   //renders the page here becoz readFile func does store the data outside of the function
+    });
 })
 
+//handle teh sub,it request to save the post 
 app.post('/submit-post',(req,res)=>{
     let postName=req.body['name'];
     let postContent=req.body['message'];
@@ -39,6 +49,21 @@ app.post('/submit-post',(req,res)=>{
 })
 
 
+//Handle the submit request of edited post
+app.post('/edit-post',(req,res)=>{
+    let postName=req.body['editname'];
+    let postContent=req.body['editmessage'];
+    fs.writeFile(`PostsFile/${postName}post.txt`,postContent,(err)=>{
+        if (err) throw err;
+        console.log(`Post Saved as ${postName}post.txt`);
+    })
+    res.redirect('/');
+})
+
+//renders the view post page 
+app.get('/view-post',(req,res)=>{
+    res.render('view-post.ejs');
+})
 
 app.listen(port,()=>{
     console.log(`Listening on ${port}`);
